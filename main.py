c80ad2087b679e1a22abec04b650ec137f1a6130
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2025-07-28 14:57:33 +0200

    Add method that counts number of tokens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 from datasets import load_dataset
 import os
+import tiktoken
 
 
 def parse_arguments():
@@ -9,18 +10,33 @@ def parse_arguments():
         "--dataset_name",
         type=str,
         default="nicohrubec/codebase-content-SWE-bench_Verified",
-        token=os.environ.get("HF_TOKEN"),
     )
     parser.add_argument("--split", type=str, default="test")
     return parser.parse_args()
 
 
 def load_data(args):
-    file_content = load_dataset(args.dataset_name, "file_content", split=args.split)
+    file_content = load_dataset(
+        args.dataset_name,
+        "file_content",
+        split=args.split,
+        token=os.environ.get("HF_TOKEN"),
+    )
     hash_to_content = {row["hash"]: row["content"] for row in file_content}
-    problem_files = load_dataset(args.dataset_name, "problem_files", split=args.split)
+    problem_files = load_dataset(
+        args.dataset_name,
+        "problem_files",
+        split=args.split,
+        token=os.environ.get("HF_TOKEN"),
+    )
 
     return file_content, hash_to_content, problem_files
+
+
+def count_tokens(string):
+    encoder = tiktoken.get_encoding("cl100k_base")  # gpt-4 tokenizer
+    tokens = encoder.encode(string)
+    return len(tokens)
 
 
 def main():
