641b8114d402f0ea882b12c24ca68818c76628ed
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2025-07-29 14:53:40 +0200

    Remove comments in dataset preprocessing

--- dataset-loader/helper.py
+++ dataset-loader/helper.py
@@ -4,6 +4,10 @@ from pathlib import Path
 import subprocess
 from typing import Dict
 import tqdm
+import tokenize
+import token
+import io
+import copy
 
 from swebench_problem import _SWEBenchProblem, CodebaseContent, FileInCodebase
 
@@ -53,6 +57,44 @@ def clone_repos(problems: list[_SWEBenchProblem], repos_dir: Path):
         assert output.returncode == 0
 
 
+# Copied from: https://gist.github.com/BroHui/aca2b8e6e6bdf3cb4af4b246c9837fa3
+def remove_comments(input_source: str) -> str:
+    source_flag = copy.copy(input_source)
+    source = io.StringIO(input_source)
+    ls = input_source.split('\n')
+    prev_toktype = token.INDENT
+    readline = source.readline
+
+    def get_char_index(lineno, col):
+        # find the index of the char in the source code
+        if lineno == 1:
+            return len('\n'.join(ls[:(lineno-1)])) + col
+        else:
+            return len('\n'.join(ls[:(lineno-1)])) + col + 1
+
+    def replace_char_between(start_lineno, start_col, end_lineno, end_col, source, replace_char, ls):
+        # replace char between start_lineno, start_col and end_lineno, end_col with replace_char, but keep '\n' and ' '
+        b = get_char_index(start_lineno, start_col)
+        e = get_char_index(end_lineno, end_col)
+        for i in range(b, e):
+            if source[i] == '\n':
+                source = source[:i] + '\n' + source[i+1:]
+            elif source[i] == ' ':
+                source = source[:i] + ' ' + source[i+1:]
+            else:
+                source = source[:i] + replace_char + source[i+1:]
+        return source
+
+    tokgen = tokenize.generate_tokens(readline)
+    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
+        if toktype == token.STRING and prev_toktype == token.INDENT:
+            source_flag = replace_char_between(slineno, scol, elineno, ecol, source_flag, ' ', ls)
+        elif toktype == tokenize.COMMENT:
+            source_flag = replace_char_between(slineno, scol, elineno, ecol, source_flag, ' ', ls)
+        prev_toktype = toktype
+    return source_flag
+
+
 def get_codebase_content(
     problem: _SWEBenchProblem, repos_dir: Path, hash_to_content: Dict[str, str]
 ) -> CodebaseContent:
@@ -82,6 +124,7 @@ def get_codebase_content(
             # Ignore these files.
             continue
 
+        content = remove_comments(content)
         content_hash = hash_file_content(content)
         if content_hash not in hash_to_content:
             hash_to_content[content_hash] = content
