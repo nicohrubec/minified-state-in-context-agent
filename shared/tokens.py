9b3a99b7795c3169330c9874ca4706765e68e6c8
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2025-08-18 13:05:09 +0200

    Add utility to count tokens for a list of files

--- shared/tokens.py
+++ shared/tokens.py
@@ -5,3 +5,12 @@ def count_tokens(string):
     encoder = tiktoken.get_encoding("cl100k_base")  # gpt-4 tokenizer
     tokens = encoder.encode(string)
     return len(tokens)
+
+
+def count_tokens_list(source_list):
+    num_tokens = 0
+
+    for source in source_list:
+        num_tokens += count_tokens(source)
+
+    return num_tokens
