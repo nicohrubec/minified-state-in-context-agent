f26d57b87e2437b3c1d92768ea2458f003688dbe
Nicolas Hrubec <nicolas.hrubec@outlook.com>
2025-08-18 12:03:18 +0200

    Add dedent and reduce_operators transformation

--- agent/minify.py
+++ agent/minify.py
@@ -6,6 +6,8 @@ from agent.transformations import (
     remove_blank_lines,
     remove_comments,
     remove_docstrings,
+    dedent,
+    reduce_operators,
 )
 
 
@@ -14,12 +16,16 @@ IMPORT_REMOVE_TRANSFORMATION_CONST = "remove_imports"
 BLANK_LINE_TRANSFORMATION_CONST = "remove_blank_lines"
 COMMENTS_REMOVE_TRANSFORMATION_CONST = "remove_comments"
 DOCSTRINGS_REMOVE_TRANSFORMATION_CONST = "remove_docstrings"
+DEDENT_TRANSFORMATION_CONST = "dedent"
+REDUCE_OPERATORS_CONST = "reduce_operators"
 DEFINED_TRANSFORMATIONS = [
     IMPORT_REMOVE_TRANSFORMATION_CONST,
     IMPORT_REMOVE_TRANSFORMATION_CONST,
     BLANK_LINE_TRANSFORMATION_CONST,
     COMMENTS_REMOVE_TRANSFORMATION_CONST,
     DOCSTRINGS_REMOVE_TRANSFORMATION_CONST,
+    DEDENT_TRANSFORMATION_CONST,
+    REDUCE_OPERATORS_CONST,
 ]
 
 
@@ -36,6 +42,10 @@ def minify(source_files: List[str], transformations: List[str]):
         source_files = remove_comments(source_files)
     if DOCSTRINGS_REMOVE_TRANSFORMATION_CONST in transformations:
         source_files = remove_docstrings(source_files)
+    if DEDENT_TRANSFORMATION_CONST in transformations:
+        source_files = dedent(source_files)
+    if REDUCE_OPERATORS_CONST in transformations:
+        source_files = reduce_operators(source_files)
 
     unknown_transformations = [
         t for t in transformations if t not in DEFINED_TRANSFORMATIONS
--- agent/transformations.py
+++ agent/transformations.py
@@ -109,6 +109,43 @@ def remove_docstrings(source_files: List[str]):
             mini.remove_docstrings(tokenized_body)
             new_sources.append(path + "\n" + token_utils.untokenize(tokenized_body))
         except (tokenize.TokenError, IndentationError):
+            print("broke")
             new_sources.append(path + "\n" + body)
 
+    return new_sources
+
+
+def dedent(source_files: List[str]):
+    source_paths = [src.splitlines()[0] for src in source_files]
+    source_bodies = ["\n".join(src.splitlines()[1:]) for src in source_files]
+
+    new_source_bodies = []
+    for src_body in source_bodies:
+        try:
+            src_body_dedent = mini.dedent(src_body)
+            new_source_bodies.append(src_body_dedent)
+        except (tokenize.TokenError, IndentationError):
+            new_source_bodies.append(src_body)
+
+    new_sources: List[str] = [
+        path + "\n" + body for path, body in zip(source_paths, new_source_bodies)
+    ]
+    return new_sources
+
+
+def reduce_operators(source_files: List[str]):
+    source_paths = [src.splitlines()[0] for src in source_files]
+    source_bodies = ["\n".join(src.splitlines()[1:]) for src in source_files]
+
+    new_source_bodies = []
+    for src_body in source_bodies:
+        try:
+            src_body_with_reduced_operators = mini.reduce_operators(src_body)
+            new_source_bodies.append(src_body_with_reduced_operators)
+        except (tokenize.TokenError, IndentationError):
+            new_source_bodies.append(src_body)
+
+    new_sources: List[str] = [
+        path + "\n" + body for path, body in zip(source_paths, new_source_bodies)
+    ]
     return new_sources
